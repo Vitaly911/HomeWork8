b055bd9596bb830fb16944d6a4232447b06a9542
Vitaly <sinicin1313@gmail.com>
2023-02-05 11:32:03 +0300

    first commit

---
+++ Задача 54/Program.cs
@@ -0,0 +1,35 @@
+﻿int[,] array = new int[3, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+int min = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = j + 1; k < array.GetLength(1); k++)
+        {
+            if (array[i, j] < array[i, k])
+            {
+                min = array[i, j];
+                array[i, j] = array[i, k];
+                array[i, k] = min;
+            }
+        }
+
+    }
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
---
+++ Задача 56/Program.cs
@@ -0,0 +1,40 @@
+﻿int[,] array = new int[3, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int[] arr = new int[array.GetLength(0)];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sum = sum + array[i, j];
+    }
+    arr[i] = sum;
+}
+for (int j = 0; j < arr.Length; j++)
+{
+    Console.Write($"{arr[j]} ");
+}
+Console.WriteLine();
+int min = 100;
+int num = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (min > arr[i])
+    {
+        min = arr[i];
+        num++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine(min);
+Console.WriteLine();
+Console.WriteLine($"Строка с наименьшей суммой элементов: {num}");
